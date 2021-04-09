8b125a4445671ce31f07196e811b1f6436f20b93
khmaker <khmaker@yandex.ru>
2021-04-09 16:42:11 +0300

    class Browser

--- Text-Based Browser/task/browser/browser.py
+++ Text-Based Browser/task/browser/browser.py
@@ -1,28 +1,74 @@
+# coding=utf-8
 import os
 import sys
-from pathlib import Path
 from argparse import ArgumentParser
+from pathlib import Path
+from shutil import rmtree
+
 import requests
 from bs4 import BeautifulSoup
-from colorama import Fore, init
+from colorama import Fore
+from colorama import init
 
 init(autoreset=True)
 
 
 class Browser:
-    def __init__(self):
-        self.parse_directory_name()
+    tags = ('p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'ul', 'ol', 'li')
 
-    @staticmethod
-    def parse_directory_name():
+    def __init__(self):
+        self.directory_name = None
+        self.__parse_directory_name()
+        self.history = []
+        self.cache = set()
+        self.url = None
+        self.current_page = None
+        self.dispatcher()
+
+    def dispatcher(self):
+        user_input = input()
+        if user_input in ('back', 'exit'):
+            {'back': self.__back, 'exit': self.__exit}.get(user_input)()
+
+    def __parse_directory_name(self):
         parser = ArgumentParser()
         parser.add_argument('directory_name')
         directory_name = parser.parse_args().directory_name
         if directory_name is not None:
             Path(directory_name).mkdir(exist_ok=True)
+            self.directory_name = directory_name
         else:
-            print('No directory name was set')
-            sys.exit(0)
+            self.__exit('No directory name was set')
+
+    def __exit(self, message=None):
+        print(message if message is not None else '')
+        rmtree(Path(self.directory_name))
+        sys.exit(0)
+
+    def make_soup(self):
+        pass
+
+    def __back(self):
+        if not self.history:
+            print('History is empty')
+        else:
+            last_history_item = self.history.pop()
+            with open(f'{self.directory_name}/{last_history_item}.txt',
+                      'r',
+                      encoding='utf-8') as file:
+                print(file.read())
+        return self.dispatcher()
+
+    def __write_file(self, soup=None):
+        # TODO: split file handling and print
+        if soup is not None:
+            with open(f'{self.directory_name}/{self.url}.txt',
+                      'w',
+                      encoding='utf-8') as file:
+                for string in soup.find_all(self.tags):
+                    text = string.get_text()
+                    file.write(text)
+                    print(Fore.BLUE + text if string.name == 'a' else text)
 
 
 def make_directory():
