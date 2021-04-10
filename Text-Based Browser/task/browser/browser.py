1393fb634322dcbe16ef01f7fd0aa5d0b08572cd
khmaker <khmaker@yandex.ru>
2021-04-10 17:58:49 +0300

    class Browser

--- Text-Based Browser/task/browser/browser.py
+++ Text-Based Browser/task/browser/browser.py
@@ -1,9 +1,7 @@
 # coding=utf-8
-import os
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
-from shutil import rmtree
 
 import requests
 from bs4 import BeautifulSoup
@@ -21,14 +19,8 @@ class Browser:
         self.__parse_directory_name()
         self.history = []
         self.cache = set()
-        self.url = None
         self.current_page = None
         self.dispatcher()
-
-    def dispatcher(self):
-        user_input = input()
-        if user_input in ('back', 'exit'):
-            {'back': self.__back, 'exit': self.__exit}.get(user_input)()
 
     def __parse_directory_name(self):
         parser = ArgumentParser()
@@ -40,100 +32,66 @@ class Browser:
         else:
             self.__exit('No directory name was set')
 
+    def dispatcher(self):
+        user_input = input()
+        if user_input in ('back', 'exit'):
+            {'back': self.__back, 'exit': self.__exit}.get(user_input)()
+        if user_input in self.cache:
+            self.__open_file(user_input)
+            return self.dispatcher()
+        self.process_user_input(user_input)
+
+    def process_user_input(self, user_input):
+        if '.' not in user_input:
+            print('Error: Incorrect URL')
+            return self.dispatcher()
+        self.__get_page(user_input)
+        self.__print_file()
+        filename, _, _ = user_input.rpartition('.')
+        self.__write_file(filename)
+        self.cache.add(filename)
+        return self.dispatcher()
+
+    def __get_page(self, user_input):
+        try:
+            self.current_page = requests.get(f'https://' + user_input).text
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return self.dispatcher()
+
     def __exit(self, message=None):
         print(message if message is not None else '')
-        rmtree(Path(self.directory_name))
         sys.exit(0)
 
     def make_soup(self):
-        pass
+        return BeautifulSoup(self.current_page, 'html.parser')
 
     def __back(self):
         if not self.history:
             print('History is empty')
         else:
-            last_history_item = self.history.pop()
-            with open(f'{self.directory_name}/{last_history_item}.txt',
-                      'r',
-                      encoding='utf-8') as file:
-                print(file.read())
+            self.current_page = self.__open_file(self.history.pop())
         return self.dispatcher()
 
-    def __write_file(self, soup=None):
-        # TODO: split file handling and print
-        if soup is not None:
-            with open(f'{self.directory_name}/{self.url}.txt',
-                      'w',
-                      encoding='utf-8') as file:
-                for string in soup.find_all(self.tags):
-                    text = string.get_text()
-                    file.write(text)
-                    print(Fore.BLUE + text if string.name == 'a' else text)
+    def __write_file(self, filename):
+        with open(f'{self.directory_name}/{filename}.txt',
+                  'w',
+                  encoding='utf-8') as file:
+            file.write(self.current_page)
 
+    def __open_file(self, filename):
+        with open(f'{self.directory_name}/{filename}.txt',
+                  'r',
+                  encoding='utf-8') as file:
+            return file.read()
 
-def make_directory():
-    global dir_name
-    parser = ArgumentParser()
-    parser.add_argument('dir')
-    dir_name = parser.parse_args().dir
-    if not os.path.exists(dir_name):
-        os.mkdir(dir_name)
-
-
-def write_file(n):
-    global current
-    name = '.'.join(n.split('.')[:-1])
-    resp = requests.get(f'https://{n}')
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    with open(f'{dir_name}/{name}.txt', 'w', encoding='utf-8') as file:
-        tags = ('p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'ul', 'ol', 'li')
-        for string in soup.find_all(tags):
+    def __print_file(self):
+        soup = self.make_soup()
+        for string in soup.find_all(self.tags):
             text = string.get_text()
-            file.write(text)
             print(Fore.BLUE + text if string.name == 'a' else text)
 
-    files.add(name)
-    if current:
-        history.append(current)
-    current = name
-    return call_url()
-
-
-def call_url():
-    n = input()
-    if '.' in n:
-        write_file(n)
-    elif n == 'exit':
-        return
-    elif n in files:
-        return call_file(n)
-    elif n == 'back':
-        return call_back()
-    else:
-        print('Error: Incorrect URL')
-        return call_url()
-
-
-def call_file(name):
-    global current
-    with open(f'{dir_name}/{name}.txt', 'r', encoding='utf-8') as file:
-        print(file.read())
-    history.append(current)
-    current = name
-    return call_url()
-
-
-def call_back():
-    if history:
-        with open(f'{dir_name}/{history.pop()}.txt', 'r', encoding='utf-8') as file:
-            print(file.read())
-        return call_url()
-
-
-make_directory()
-files = set()
-history = []
-current = ''
 
 if __name__ == '__main__':
-    call_url()
+    # call_url()
+    Browser()
